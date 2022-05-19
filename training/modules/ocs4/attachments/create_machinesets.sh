5b36777e214cd82a7ba50d66d9b9ab8ca1abea8b
Daniel Parkes <dparkes@redhat.com>
2022-05-19 07:38:38 +0200

    Closes #185 .Fixed create_machinesets.sh script so the resulting machineset name has workerocs instead of ocs

--- training/modules/ocs4/attachments/create_machinesets.sh
+++ training/modules/ocs4/attachments/create_machinesets.sh
@@ -12,11 +12,8 @@ for MACHINESET in $(oc get -n openshift-machine-api machinesets -o name | grep -
 do
   oc get -n openshift-machine-api "$MACHINESET" -o json | jq '
       del( .metadata.uid, .metadata.managedFields, .metadata.selfLink, .metadata.resourceVersion, .metadata.creationTimestamp, .metadata.generation, .status) | 
-
-      (.metadata.name, .spec.selector.matchLabels["machine.openshift.io/cluster-api-machineset"], .spec.template.metadata.labels["machine.openshift.io/cluster-api-machineset"]) |= sub("worker";"ocs") | 
-
+      (.metadata.name, .spec.selector.matchLabels["machine.openshift.io/cluster-api-machineset"], .spec.template.metadata.labels["machine.openshift.io/cluster-api-machineset"]) |= sub("worker";"workerocs") | 
       (.spec.template.spec.providerSpec.value.instanceType) |= "m5.4xlarge" |
-
       (.spec.template.spec.metadata.labels["cluster.ocs.openshift.io/openshift-storage"]) |= ""' | oc apply -f -
 done
 
